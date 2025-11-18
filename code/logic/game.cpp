4e03ec9b99c1eb8566753df5d2e66828e95f825c
Matheus Alexandre <matheus.sa15@aluno.ifsc.edu.br>
2025-11-17 22:07:53 -0300

    add game logic

--- code/logic/game.cpp
+++ code/logic/game.cpp
@@ -1,1 +1,69 @@
 #include "game.h"
+
+Game::Game(MatchMode mode)
+    : mode(mode), playerWins(0), computerWins(0), roundsPlayed(0) {}
+
+void Game::resetGame() {
+    playerWins = 0;
+    computerWins = 0;
+    roundsPlayed = 0;
+}
+
+std::string Game::playRound(const Move& player, const Move& computer) {
+    roundsPlayed++;
+
+    std::string text = "You chose " + player.getName() +
+                       ", Computer chose " + computer.getName() + ". ";
+
+    // Emit event to QT
+
+    if (player.defeats(computer)) {
+        playerWins++;
+        text = "You win this round!";
+        // Emit event to QT
+    } else if (computer.defeats(player)) {
+        computerWins++;
+        text = "Computer wins this round!";
+        // Emit event to QT
+    } else {
+        text = "It's a tie"
+        // Emit event to QT
+    }
+
+    if (mode == MatchMode::SINGLE) {
+        result += getFinalResult();
+        // Emit event to QT
+    }
+    else if (mode == MatchMode::BEST_OF_THREE) {
+        if (playerWins == 2 || computerWins == 2) {
+            result += getFinalResult();
+        } else {
+            // Emit event to QT
+        }
+    }
+
+    return result;
+}
+
+bool Game::isFinished() const {
+    if (mode == MatchMode::SINGLE) {
+        return roundsPlayed >= 1;
+    }
+    if (mode == MatchMode::BEST_OF_THREE) {
+        return (playerWins == 2 || computerWins == 2);
+    }
+    return false;
+}
+
+std::string Game::getFinalResult() const {
+    if (playerWins > computerWins) {
+        // Emit event to QT
+        text = "Game Result: You win the match!";
+    } else if (computerWins > playerWins) {
+        // Emit event to QT
+        text = "Game Result: Computer wins the match!";
+    } else {
+        // Emit event to QT
+        text = "Game Result: It's a draw!";
+    }
+}
--- code/logic/game.h
+++ code/logic/game.h
@@ -1,4 +1,29 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "move.h"
+#include <string>
+
+enum class MatchMode {
+    SINGLE,
+    BEST_OF_THREE
+};
+
+class Game {
+public:
+    MatchMode mode = MatchMode::SINGLE;
+
+    std::string playRound(const Move& player, const Move& computer);
+
+    bool isFinished() const;
+    std::string getFinalResult() const;
+
+    void resetGame();
+private:
+    MatchMode mode;
+    int playerWins;
+    int computerWins;
+    int roundsPlayed;
+};
+
 #endif // GAME_H
