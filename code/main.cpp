3f478fc1553f876025c8f74c251ebcb02fe5a8f2
Matheus Alexandre <matheus.sa15@aluno.ifsc.edu.br>
2025-11-24 22:11:25 -0300

    use mainwindow in main file

--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,11 @@
-#include "rpsls.h"
+#include "./ui/mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    RPSLS w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
